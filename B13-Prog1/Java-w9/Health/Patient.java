be78992bcbc4fd4c9581ac0e65bf90cf39de05af
Alex <a.atanasov2004@outlook.com>
2023-11-09 00:28:52 +0100

    week 9 structure

--- B13-Prog1/Java-w9/Health/Patient.java
+++ B13-Prog1/Java-w9/Health/Patient.java
@@ -1,7 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+
 public class Patient {
+    private int fromAge;
+    private int toAge;
+    private String sex;
+    private int day;
+    private int month;
+    private int year;
+
     private Patient() {}
 
     public static Patient parseCSVLine(String line) {
+        
+        
         return null;
+    }
+
+    public int getFromAge() {
+        return fromAge;
+    }
+
+    public int getToAge() {
+        return toAge;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
