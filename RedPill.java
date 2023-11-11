d1660a0eb342b0635b837dd3c4221648ce476d9d
Alex <a.atanasov2004@outlook.com>
2023-11-12 00:49:00 +0100

    going into the matrix

---
+++ RedPill.java
@@ -0,0 +1,29 @@
+public class RedPill {
+
+    public static void matrixTyping(String phrase) {
+        StringBuilder phraseBuilder = new StringBuilder("");
+        char symbol = ' ';
+
+        for (int i = 0; i <= phrase.length() - 1; i++) {
+            symbol = ' ';
+            for (int j = 0; j <= 95; j++) {
+                System.out.print(phraseBuilder);
+                System.out.println(symbol);
+                if (phrase.charAt(i) == symbol) break;
+                symbol++;
+                try {
+                    Thread.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            phraseBuilder.append(symbol);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String dickheadString = "Eat my shorts!";
+        matrixTyping(dickheadString);
+    }
+}
