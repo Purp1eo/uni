158f63aa082c06d2ad1171d24af5c0bd9692d3bc
Alex <a.atanasov2004@outlook.com>
2023-11-17 17:12:10 +0100

    added all versions of printHarmonicSeriesFormula

--- B13-Prog1/Java-w4/harmony.java
+++ B13-Prog1/Java-w4/harmony.java
@@ -10,6 +10,9 @@ public class harmony {
     }
 
     public static void printHarmonicSeriesFormula(int n) {
+        
+        /////////////////////////////////////////////////////////////////////// using a StringBuilder
+        
         StringBuilder fractions = new StringBuilder("1");
         
         if (n <= 0) {}
@@ -22,9 +25,41 @@ public class harmony {
             fractions.append(" = ").append(getHarmonicSeriesAt(n));
             System.out.println(fractions);
         }
+
+        /////////////////////////////////////////////////////////////////////// using a normal String
+        
+        /* if (n <= 0) {}
+        else if (n == 1) System.out.println(1);
+        else {
+            System.out.print(1 + " ");
+            for (int i = 2; i <= n; i++) {
+                System.out.print("+ (1/"+ i + ") ");
+                if (i % 10 == 0) System.out.print("\n");
+            }
+            System.out.println("= " + getHarmonicSeriesAt(n));
+        } */
+
+        /////////////////////////////////////////////////////////////////////// using Daniel's Code TM
+        
+        /* if (n <= 0) {
+            System.out.print("");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (i % 10 == 0) {
+                    System.out.print(" + (1/" + i + ")");
+                    System.out.print("\n");
+                } else if (i == n && i > 1) {
+                    System.out.print(" + " + "(1/" + i + ") = " + getHarmonicSeriesAt(n));
+                } else if (i == 1) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(" + " + "(1/" + i + ")");
+                }
+            }
+        } */
     }
 
     public static void main(String[] args) {
-            printHarmonicSeriesFormula(13);
+            printHarmonicSeriesFormula(15);
     }
 }
