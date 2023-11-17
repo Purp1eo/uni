dea6e7c97f9a6f0222025a5d091eec1c12988aa0
Alex <a.atanasov2004@outlook.com>
2023-11-17 22:06:35 +0100

    csv reading functionality implemented

--- B13-Prog1/Java-w9/Health/Patient.java
+++ B13-Prog1/Java-w9/Health/Patient.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.File;
+import java.io.IOException;
 
 public class Patient {
     private int fromAge;
@@ -14,7 +14,44 @@ public class Patient {
 
     public static Patient parseCSVLine(String line) {
         
+        String csvFile = "C:/Users/aatan/OneDrive/Documents/latestdata.csv";
+        String attributeLine = "";
+        String fileDelimiter = ",";
+
+        Patient person = new Patient();
         
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((attributeLine = br.readLine()) != null) {
+    
+                String[] attributes = attributeLine.split(fileDelimiter);
+                
+                String ageValue = attributes[1];
+                person.sex = attributes[2];
+                String dateOnset = attributes[11];
+
+                if (ageValue == null) {
+                    person.fromAge = -1;
+                    person.toAge = -1;
+                } else if (ageValue.contains("-")) {
+                    String[] ageRangeNumbers = ageValue.split("-");
+                    person.fromAge = Integer.parseInt(ageRangeNumbers[0]);
+                    person.toAge = Integer.parseInt(ageRangeNumbers[1]);
+                } else {
+                    person.fromAge = Integer.parseInt(ageValue);
+                    person.toAge = Integer.parseInt(ageValue);
+                }
+
+                String[] onsetDates = dateOnset.split(".");
+                person.day = Integer.parseInt(onsetDates[0]);
+                person.month = Integer.parseInt(onsetDates[1]);
+                person.year = Integer.parseInt(onsetDates[2]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -40,5 +77,9 @@ public class Patient {
 
     public int getYear() {
         return year;
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
