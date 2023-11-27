439230ddaa8024abb6466dda17db2ef31906d036
Alex <80852716+Purp1eo@users.noreply.github.com>
2023-11-27 01:34:12 +0100

    added monthlySales method to week9/health/test.java

---
+++ B13-Prog1/Java-w9/Culture/test.java
@@ -0,0 +1,24 @@
+public class test {
+    public static int[] monthlySales(ArtAuctionList pl, int fromYear, int toYear, int fromMonth, int toMonth) {
+
+        int numMonths = ((toYear - fromYear) * 12) + (toMonth - fromMonth) + 1;
+        int[] salesPerMonth = new int[numMonths];
+
+        int index = 0;
+        for (int year = fromYear; year <= toYear; year++) {
+            int startMonth = (year == fromYear) ? fromMonth : 1;
+            int endMonth = (year == toYear) ? toMonth : 12;
+
+            for (int month = startMonth; month <= endMonth; month++) {
+                SalesList filteredByYearAndMonth = pl.filterByYear(year).filterByMonth(month);
+                int sales = filteredByYearAndMonth.getNumSales();
+                salesPerMonth[index++] = sales;
+            }
+        }
+
+        return salesPerMonth;
+    }
+    public static void main(String[] args) {
+
+    }
+}
