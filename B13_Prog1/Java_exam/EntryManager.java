7903c297926e0d0e1a9efeb4ad131198ba10b21b
Pablo <a.atanasov2004@outlook.com>
2024-02-10 00:25:14 +0100

    entrymanager done

--- B13_Prog1/Java_exam/EntryManager.java
+++ B13_Prog1/Java_exam/EntryManager.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class EntryManager {
     
-    private String populationFilename;
-    private String HIVfilename;
     private String country;
+    private ArrayList<PopulationEntry> population = new ArrayList<PopulationEntry>();
+    private ArrayList<HIVEntry> HIVDeaths = new ArrayList<HIVEntry>();
+    private ArrayList<HIVEntry> HIVPrevalence = new ArrayList<HIVEntry>();
 
     public EntryManager(String populationFilename, String HIVfilename, String country) {
-
+        this.country = country;
+        addFromCSVFile(populationFilename);
+        addFromCSVFile(HIVfilename);
     }
     
     public void addFromCSVFile(String filepath) {
@@ -18,38 +22,94 @@ public class EntryManager {
 
             br.readLine(); // skip header
             while ((line = br.readLine()) != null) {
-                addPatient(Patient.parseCSVLine(line));
+                if(filepath.equals("populationFilename")) population.add(parsePopulation(line));
+                if(filepath.equals("HIVfilename")) {
+                    HIVDeaths.add(parseHIVDeaths(line));
+                    HIVPrevalence.add(parseHIVPrevelance(line));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public populationEntry getPopulationEntryForYear(int year) {
+    public PopulationEntry parsePopulation(String line) {
+                
+        String fileDelimiter = ",";
+        String[] attributes = line.split(fileDelimiter);
         
-       // return entry from year 
+        String location = attributes[1];
+        if(!country.equals(location)) return null;
+        int fromYear = Integer.parseInt(attributes[4].split("-")[0]);
+        int toYear = Integer.parseInt(attributes[4].split("-")[1]);
+        double deathRate = Double.parseDouble(attributes[15]);
         
+        PopulationEntry pop = new PopulationEntry(location, fromYear, toYear, deathRate);
+
+        return pop;
+    }
+
+    public HIVEntry parseHIVDeaths(String line) {
+        
+        String fileDelimiter = ",";
+        String[] attributes = line.split(fileDelimiter);
+        
+        String location = attributes[3];
+        if(!country.equals(location)) return null;
+        String measure = attributes[1];
+        int year = Integer.parseInt(attributes[12]);
+        double value = Double.parseDouble(attributes[13]);
+
+        HIVEntry hivd = new HIVEntry(measure, year, value);
+    
+        return hivd;
+    }
+
+    public HIVEntry parseHIVPrevelance(String line) {
+
+        String fileDelimiter = ",";
+        String[] attributes = line.split(fileDelimiter);
+
+        String location = attributes[3];
+        if(!country.equals(location)) return null;
+        String measure = attributes[1];
+        int year = Integer.parseInt(attributes[12]);
+        double value = Double.parseDouble(attributes[13]);
+
+        HIVEntry hivp = new HIVEntry(measure, year, value);
+    
+        return hivp;
+    }
+
+    public PopulationEntry getPopulationEntryForYear(int year) {
+        
+        for (PopulationEntry element : population) {
+           if(element.isValidForYear(year)) return element;
+        } 
+
         return null;
     }
 
     public HIVEntry getHIVDeathrateForYear(int year) {
 
-        // return entry for "death" measure from year
+        for (HIVEntry element : HIVDeaths) {
+           if(element.getYear() == year) return element;
+        } 
 
         return null;
     }
 
     public HIVEntry getHIVPrevalenceForYear(int year) {
 
-        // return entry for "prevalence" measure from year
+        for (HIVEntry element : HIVPrevalence) {
+           if(element.getYear() == year) return element;
+        } 
 
         return null;
     }
 
     public String getCountry() {
         
-        // return country, for which entry was made
-
-        return null;
+        return country;
     }
 }
