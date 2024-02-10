d333ebd1187a142a319b8d34228d95dad5e929d0
Pablo <a.atanasov2004@outlook.com>
2024-02-10 15:12:26 +0100

    added getter

--- B13_Prog1/Java_exam/Analysis.java
+++ B13_Prog1/Java_exam/Analysis.java
@@ -1,21 +1,66 @@
 public class Analysis {
-    public Analysis(String populationFilename, String HIVfilename, String country) {
 
+    private EntryManager managingManager;
+
+    public Analysis(String populationFilename, String HIVfilename, String country) {
+        
+        managingManager = new EntryManager(populationFilename, HIVfilename, country);
     } 
 
     public double[] getCrudeDeathrates(int fromYear, int toYear) {
-        return null;
+        
+        double[] CDRs = new double[(toYear-fromYear)+1];
+        
+        int index = 0;
+        for (int i = fromYear; i <= toYear; i++) {
+            CDRs[index] = (managingManager.getPopulationEntryForYear(i) == null) ? 0 : managingManager.getPopulationEntryForYear(i).getDeathRate() * 100;
+            index++;
+        }
+
+        return CDRs;
     }
 
     public double[] getHIVDeathrates(int fromYear, int toYear) {
-        return null;
+        
+        double[] HIVDs = new double[(toYear-fromYear)+1];
+        
+        int index = 0;
+        for (int i = fromYear; i <= toYear; i++) {
+            HIVDs[index] = (managingManager.getHIVDeathrateForYear(i) == null) ? 0 : managingManager.getHIVDeathrateForYear(i).getValue();
+            index++;
+        }
+
+        return HIVDs;
     }
 
     public double[] getHIVPrevalences(int fromYear, int toYear) {
-        return null;
+
+        double[] HIVPs = new double[(toYear-fromYear)+1];
+        
+        int index = 0;
+        for (int i = fromYear; i <= toYear; i++) {
+            HIVPs[index] = (managingManager.getHIVPrevalenceForYear(i) == null) ? 0 : managingManager.getHIVPrevalenceForYear(i).getValue();
+            index++;
+        }
+
+        return HIVPs;
     }
 
     public double[] getPercentageHIVDeathrates(int fromYear, int toYear) {
-        return null;
+        
+        double[] PHIVDs = new double[(toYear-fromYear)+1];
+
+        double[] HIVDs = getHIVDeathrates(fromYear, toYear);
+        double[] CDRs = getCrudeDeathrates(fromYear, toYear);
+
+        for (int i = 0; i <= PHIVDs.length; i++) {
+            if(HIVDs[i] == 0.0 || CDRs[i] == 0.0) {
+                PHIVDs[i] = 0;
+                continue;
+            }
+            PHIVDs[i] = HIVDs[i] / CDRs[i] * 100;
+        }
+
+        return PHIVDs;
     }
 }
